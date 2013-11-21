92ea8048905ae3afe090b043f19507a2d3dc1710
Bruce Steedman <bruce.steedman@gmail.com>
2013-11-21 19:06:45 +0000

    User can pick up and drop off a working bike at a docking station

--- lib/user.rb
+++ lib/user.rb
@@ -1,10 +1,24 @@
 class User
 
-  attr_reader :location
+  attr_accessor :location, :bike
+
+  def initialize
+    @bike = nil
+    @location = nil
+  end
 
   def go_to(new_location)
     raise "Can't go to #{new_location}" if !(new_location.is_a? DockingStation)
     @location = new_location
+    if has_bike?
+      @location.dock(bike)
+    else
+      @bike = location.release_a_working_bike
+    end
   end
 
-end
+  def has_bike?
+    @bike
+  end
+
+end # end of describe
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -4,16 +4,39 @@ describe User do
 
   let(:station) { DockingStation.new(capacity: 20) }
   let(:user) { User.new }
-
-  it "should be able to go to a docking station" do
-    user.go_to(station)
-    expect(user.location).to eq(station)
-
-  end
+  let(:bike) { Bike.new }
+  let(:user_with_bike) { station.dock(bike); user.go_to(station); user }
 
   it "should not be able to go somewhere that isn't a docking station" do
     expect(lambda { user.go_to("Moon")}).to raise_error(RuntimeError)
+  end
 
+  it "should be able to take a bike, if a station has one" do
+    station.dock(bike)
+    expect(station.bike_count).to eq(1)
+    expect(station.available_bikes).to eq([bike])
+    expect(user.bike).to be_nil
+    expect(station.available_bikes.count).to eq(1)
+    user.go_to(station)
+    expect(user.bike).to eq(bike)
+    expect(station.bike_count).to eq(0)
+  end
+
+  it "should not be able to take a bike, if a station has none" do
+    bike.break
+    station.dock(bike)
+    expect(station.bike_count).to eq(1)
+    user.go_to(station)
+    expect(station.bike_count).to eq(1)
+  end
+
+  it "should be able to return a working bike to a docking station" do
+    station.dock(bike)
+    user.go_to(station)
+    expect(user.bike).to eq(bike)
+    expect(station.bike_count).to eq(0)
+    user.go_to(station)
+    expect(station.bike_count).to eq(1)
   end
 
 end # of describe
